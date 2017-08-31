0371bddbac5b29edbd95eb7f0d68f0694cd58fb6
marvin holleman <marvin_blabla@MacBook-Pro-van-marvin.local>
2017-08-31 15:42:22 +0200

    Webpack.config toegevoegd

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ router.get('/', photoController.homePage);
 
 router.get('/:page', (req, res) => {
     const page = req.params.page;
-    res.send('welkom op de ' + page + ' pagina');
+    res.send(`welkom op de ${page} pagina`);
 });
 
 module.exports = router;
